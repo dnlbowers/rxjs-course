d352cbdbb5184859005cb20817df48804885fdab
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-04-01 16:44:34 +0200

    Complete intro to rxjs section of course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, fromEvent, interval, noop, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,60 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    
+
+    const http$ = Observable.create(observer => {
+      fetch(`/api/courses`)
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(err => {
+        observer.error(err);
+      })
+    });
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+
+    // Example of streams without rxjs
+    // document.addEventListener('click', evt => {
+    //   console.log(evt)
+    //   setTimeout(() => {
+    //     let counter = 0;
+
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //     console.log('Timeout completed...');
+    //   }, 3000);
+    // });
+
+    // Example of streams with rxjs
+    // const interval$ = timer(3000, 1000);
+
+    // const sub = interval$.subscribe(val => console.log("stream 1: " + val));
+
+    // setTimeout(() => sub.unsubscribe(), 5000);
+
+    // const click$ = fromEvent(document, 'click')
+
+    // click$.subscribe(
+    //   evt => console.log(evt),
+    //   err => console.log(err),
+    //   () => console.log('completed')
+    //   );
+
+
+
   }
 
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -50,7 +50,7 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     }
 
-
+    save() {}
 
     close() {
         this.dialogRef.close();
