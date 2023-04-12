b4dca91b75571973ed91feb27398a92c8bb52f54
David Bowers <dbowers@greentube.com>
2023-04-12 17:04:08 +0200

    cover merge map lesson

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Observable, concat, fromEvent, interval, noop, of, timer } from 'rxjs';
+import { Observable, concat, fromEvent, interval, merge, noop, of, timer } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 import { map } from 'rxjs/operators';
 
@@ -14,13 +14,23 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const source1$ = of(1,2,3);
-    const source2$ = of(4,5,6);
-    const source3$ = of(7,8,9);
+    //example of merge
+    // const interval1$ = interval(1000);
 
-    const result$ = concat(source1$, source2$, source3$);
+    // const interval2$ = interval1$.pipe(map(val => 10 * val))
 
-    result$.subscribe(console.log);
+    // const result$ = merge(interval1$, interval2$);
+
+    // result$.subscribe(console.log);
+
+    //example of concat
+    // const source1$ = of(1,2,3);
+    // const source2$ = of(4,5,6);
+    // const source3$ = of(7,8,9);
+
+    // const result$ = concat(source1$, source2$, source3$);
+
+    // result$.subscribe(console.log);
 
     // const http$ = createHttpObservable('/api/courses')
 
